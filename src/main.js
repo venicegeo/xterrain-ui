0bec4eaad4ab1282e87f6980416f004cf296ef18
David Bazile <david.bazile@digitalglobe.com>
2017-07-06 14:02:34 -0400

    Add global styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import 'leaflet/dist/leaflet.css'
 import 'font-awesome/css/font-awesome.css'
 
+import './styles/globals.css'
+
 import 'babel-polyfill'
 
 import Vue from 'vue'
@@ -12,7 +14,7 @@ import {createStore} from './store/index'
 import Application from './components/Application.vue'
 
 window.__vm__ = new Vue({
-    el: '#Application-placeholder',
+    el: '#Application',
     store: createStore(),
     render: h => h(Application),
 })
